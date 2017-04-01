4aa0124dde5ca5b0fdb59513c601f93ae84d8cad
RryLee <qq215672398@126.com>
2017-04-01 09:59:00 +0800

    FIX 方法名 error

--- include/helper.php
+++ include/helper.php
@@ -9,19 +9,19 @@ if (! function_exists('true_id')) {
 }
 
 if (! function_exists('trueId')) {
-    function true_id($publicId, $connection = null) {
+    function trueId($publicId, $connection = null) {
         return Hashids::trueId($publicId, $connection);
     }
 }
 
 if (! function_exists('public_id')) {
-    function true_id($id, $connection = null) {
+    function public_id($id, $connection = null) {
         return Hashids::publicId($id, $connection);
     }
 }
 
 if (! function_exists('publicId')) {
-    function true_id($id, $connection = null) {
+    function publicId($id, $connection = null) {
         return Hashids::publicId($id, $connection);
     }
 }
